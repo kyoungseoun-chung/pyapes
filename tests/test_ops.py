8dce07eccff0750ff4de424a1c1d5247a05d712f
Kyoungseoun Chung <kyoungseoun.chung@gmail.com>
2023-01-03 23:39:28 +0100

    test: :white_check_mark: x and y directions are all tested.

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Test roll vs conv1d discretization"""
+import time
+
 import torch
 from torch.nn.functional import conv1d
 from torch.testing import assert_close  # type: ignore
@@ -7,9 +9,9 @@ from torch.testing import assert_close  # type: ignore
 torch.set_default_dtype(torch.float64)
 
 
-def test_ops_comp():
+def test_ops_comp_grad():
 
-    nx = int(2**4)
+    nx = int(2**12)
 
     x = torch.linspace(0, 1, nx)
     y = torch.linspace(0, 1, nx)
@@ -19,34 +21,44 @@ def test_ops_comp():
 
     X, Y = torch.meshgrid(x, y, indexing="ij")
 
-    test_field = X**2 + Y**2
+    test_field = X**2 + 2 * Y**2
 
     grad_roll_x = (
         torch.roll(test_field, -1, 0) - torch.roll(test_field, 1, 0)
     ) / (2 * dx)
+    tic = time.perf_counter()
     grad_roll_y = (
         torch.roll(test_field, -1, 1) - torch.roll(test_field, 1, 1)
     ) / (2 * dy)
+    t_roll = time.perf_counter() - tic
 
     assert_close(grad_roll_x[1:-1, 1:-1], 2 * X[1:-1, 1:-1])
-    assert_close(grad_roll_y[1:-1, 1:-1], 2 * Y[1:-1, 1:-1])
+    assert_close(grad_roll_y[1:-1, 1:-1], 4 * Y[1:-1, 1:-1])
 
     k_cw = 4
+
     # Shape = (out_channels, in_channels, kernel_size)
     test_kernel = torch.zeros((k_cw, k_cw, 3))
-
     test_kernel[0, -1, 0] = -1
     test_kernel[-1, 0, -1] = 1
-
     diag = torch.ones(k_cw - 1)
     test_kernel[:, :, 1] = torch.diag(diag, 1) - torch.diag(diag, -1)
 
-    test_kernel /= 2 * dx
+    test_kernel /= 2 * dy
 
     # Shape = (batch, in_channels, width)
-    # test_field_T = test_field.view(nx, k_cw, -1)
-    test_field_T = torch.transpose(test_field.view(nx, k_cw, -1), 1, 2)
-
+    tic = time.perf_counter()
+    test_field_T = torch.transpose(test_field.view(nx, -1, k_cw), 1, 2)
     conv_op = conv1d(test_field_T, test_kernel, stride=1, padding="same")
     grad_y = torch.reshape(torch.transpose(conv_op, 1, 2), (nx, nx))
-    assert_close(grad_y[1:-1, 1:-1], 2 * Y[1:-1, 1:-1])
+    t_conv_y = time.perf_counter() - tic
+
+    assert_close(grad_y[1:-1, 1:-1], 4 * Y[1:-1, 1:-1])
+
+    tic = time.perf_counter()
+    test_field_T = torch.transpose(test_field.T.view(nx, -1, k_cw), 2, 1)
+    conv_op = conv1d(test_field_T, test_kernel, stride=1, padding="same")
+    grad_x = torch.reshape(torch.transpose(conv_op, 2, 1), (nx, nx)).T
+    t_conv_x = time.perf_counter() - tic
+
+    assert_close(grad_x[1:-1, 1:-1], 2 * X[1:-1, 1:-1])
