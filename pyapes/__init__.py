d00b2e1c0ee9ba199f57e3944daeddb44916c07d
Kyoungseoun Chung <kyoungseoun.chung@gmail.com>
2023-02-28 21:00:08 +0100

    chore: :hammer: remove some meta data and bump the version to 0.0.2.

--- pyapes/__init__.py
+++ pyapes/__init__.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 
-__author__ = "Kyoungseoun Chung"
-__email__ = "kchung@student.ethz.ch"
-__copyright__ = "Copyright 2023, IFD, ETH Zurich"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
